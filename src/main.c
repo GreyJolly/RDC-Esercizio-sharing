e5db71d87c68f5dac33d57f52985a013fb7b9bee
GreyJolly <vortex.minecraft@libero.it>
2024-03-20 17:15:23 +0100

    Made most of the program

--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@
 #define PORT 2507
 #define BROADCAST_ADDRESS "255.255.255.255"
 #define NUMBER_OF_NODES 10
+#define SLIDING_WINDOW_SIZE 2
+#define TIME_BETWEEN_GENERATED_PACKAGES 10000
+#define TIME_BETWEEN_PRINTS 1
 
 typedef struct message {
 	int sender_id, data, sequence_number;
@@ -29,45 +32,98 @@ struct node_package {
 	node_package* previous;
 } ;
 
-typedef struct linked_list {
-	node_package* head;
-	node_package* tail;
-} linked_list;
-
 void handle_error(char * error_message) {
 	perror(error_message);
 	exit(EXIT_FAILURE);
 }
 
 // Errors in this program are sparcely handled.
-// I would have liked to have used different files but I'm not sure how to appropiately modify the Docker file
+// I would have liked to have used different files but I'm not sure how to appropiately modify the Dockerfile
 
+void delete_list(node_package * node) {
+	if (node != NULL) {
+		delete_list(node->next);
+		free(node);
+	}
+}
+
+int count_sender_in_list(node_package * node, int sender_id) {
+	if (node == NULL) return 0; 
+	return ((node->sender_id == sender_id)?1:0) + count_sender_in_list(node->next, sender_id);
+}
 
 void traffic_analyzer(int* pipe) {
+	int ret, sender_id;
+	long int timer, time_of_last_print = time(NULL);
+	message message;
+	ret = close(pipe[1]);
+	if (ret < 0) handle_error("close");
 	
-	_exit(EXIT_SUCCESS);
+	node_package *head = NULL, *tail = NULL, *new_node, *old_tail;
+	while (1) {
+		// If enough time has passed, print the current status
+		if (time(&timer) - time_of_last_print > TIME_BETWEEN_PRINTS) {
+			for (int i = 0; i<NUMBER_OF_NODES; i++) {
+				printf("%d: %03d ", i, count_sender_in_list(head, i));
+			}
+			printf("\n");
+			fflush(stdout);
+			
+			time_of_last_print = timer;
+		}
+		ret = read(pipe[0], &sender_id, sizeof(sender_id));
+		if (ret < 0) handle_error("read");
+		if (ret == 0) {
+			delete_list(head);
+			_exit(EXIT_SUCCESS);
+		}
+
+		// Set up new node and add it as head
+		new_node = malloc(sizeof(node_package));
+		new_node->timestamp = time(&timer);
+		new_node->sender_id = sender_id;
+		new_node->next = head;
+		new_node->previous = NULL;
+		if (head == NULL) tail = new_node;
+		else head->previous = new_node;
+		head = new_node;
+
+		// Delete old nodes from the list
+		while (timer - tail->timestamp > SLIDING_WINDOW_SIZE) {
+			old_tail = tail;
+			tail->previous->next = NULL;
+			tail = tail->previous;
+			free(old_tail); 
+		} 
+			
+	}
+	
+	// We should never get here
+	handle_error("Unexpectedly got somewhere impossible");
 }
 
 void traffic_generator(int* pipe) {
-	int ret; 
-	
-	srand(time(NULL));
 
+	int ret; 
+	srand(time(NULL));
 	ret = close(pipe[0]);
 	if (ret < 0) handle_error("close");
 
 	while (1) {
-		usleep(1000000);
+		usleep(TIME_BETWEEN_GENERATED_PACKAGES);
 		int data = rand();
-		write(pipe[0], &data, sizeof(data));
+		ret = write(pipe[1], &data, sizeof(data));
+		if (ret < 0) handle_error("write");
+		if (ret == 0) _exit(EXIT_SUCCESS); 
 	}
 
-	_exit(EXIT_SUCCESS);
+	// We should never get here
+	handle_error("Unexpectedly got somewhere impossible");
 }
 
 void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 
-	int ret; 
+	int ret;
 
 	// OPEN LISTENING SOCKET
 
@@ -86,6 +142,8 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 	listening_address.sin_port = htons(PORT);
 
 	bind(listening_fd, (struct sockaddr *) &listening_address, sizeof(listening_address));
+
+	fcntl(listening_fd, F_SETFL, O_NONBLOCK); //Set read as non blocking
 
 	// OPEN WRITING SOCKET
 	
@@ -117,17 +175,15 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 
 	while(1) {
 
-		printf("Ndebuggg\n");
-		fflush(stdout);
-
 		// We check whether we have a new message ready (the read is non-blocking);
 		ret = read(generator_pipe[0], &data, sizeof(data));
+
 		if (ret > 0) {
-			printf("RICEVUTO MESSAGGIO DA INVIARE: %d\n", data);
-			fflush(stdout);
 
-			internal_sequence_number = 0;
-
+			// Tell the analyzer that we recieved some data
+			ret = write(analyzer_pipe[1], &node_id, sizeof(node_id));
+			if (ret <= 0) handle_error("write");
+			
 			sending_message.data = data;
 			sending_message.sender_id = node_id;
 			sending_message.sequence_number = 0;
@@ -137,25 +193,26 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 		}
 
 		ret = read(listening_fd, &recieving_message, sizeof(message));
+		if (ret < 0) continue;
 		if (ret < sizeof(message)) handle_error("read");
-	
+
 		sender_id = recieving_message.sender_id;
 		sequence_number = recieving_message.sequence_number;
 		data = recieving_message.data;
-		
-		printf("sender_id == %d\nnode_id == %d\n", sender_id, node_id);
+
 		if (sender_id == node_id) continue;
 		// If the package's sequence number is higher than the internal one, discard it.
 		if (sequence_number>=internal_sequence_number && internal_sequence_number != -1) continue;
-				
+
+		// Tell the analyzer that we recieved some data
+		ret = write(analyzer_pipe[1], &sender_id, sizeof(sender_id));
+		if (ret <= 0) handle_error("write");
+
 		internal_sequence_number = sequence_number;
 
 		sending_message.data = data;
 		sending_message.sender_id = node_id;
 		sending_message.sequence_number = ++internal_sequence_number;
-
-		printf("Recieved data package: %d\n", data);
-		fflush(stdout);
 
 		ret =sendto(writing_fd, &sending_message, sizeof(message), 0, (struct sockaddr*) &broadcast_addr, sizeof(broadcast_addr));
 		if (ret < 0) handle_error("sendto");
@@ -172,25 +229,24 @@ int main(int argc, char* argv[]) {
 	}
 
 	int ret, generator_pipe[2], analyzer_pipe[2];
-	fcntl(generator_pipe[1], F_SETFL, O_NONBLOCK); //Set read as non blocking
 
 	ret = pipe(generator_pipe);
 	if (ret < 0) handle_error("pipe");
-	ret = close(generator_pipe[1]);
-	if (ret < 0) handle_error("close");
-
+	fcntl(generator_pipe[0], F_SETFL, O_NONBLOCK); //Set read as non blocking
 	ret = pipe(analyzer_pipe);
 	if (ret < 0) handle_error("pipe");
+
+	ret = fork();
+	if (ret < 0) handle_error("fork");
+	if (ret > 0) traffic_analyzer(analyzer_pipe);
 	ret = close(analyzer_pipe[0]);
 	if (ret < 0) handle_error("close");
 
 	ret = fork();
 	if (ret < 0) handle_error("fork");
-	if (ret > 0) traffic_analyzer(analyzer_pipe);
-	
-	ret = fork();
-	if (ret < 0) handle_error("fork");
 	if (ret > 0) traffic_generator(generator_pipe);
+	ret = close(generator_pipe[1]);
+	if (ret < 0) handle_error("close");
 
 	broadcaster(atoi(argv[1]), generator_pipe, analyzer_pipe);
 }
