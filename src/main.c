7c67b32fb07457bf06f4ba2c3dfac979bda9ebce
GreyJolly <vortex.minecraft@libero.it>
2024-03-21 09:54:50 +0100

    Fixed up code

--- src/main.c
+++ src/main.c
@@ -9,119 +9,164 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
+#include <ifaddrs.h>
 
 #define BUFFER_SIZE 64
 #define MAXIMUM_QUEUE 8
 #define PORT 2507
 #define BROADCAST_ADDRESS "255.255.255.255"
 #define NUMBER_OF_NODES 10
-#define SLIDING_WINDOW_SIZE 2
-#define TIME_BETWEEN_GENERATED_PACKAGES 10000
-#define TIME_BETWEEN_PRINTS 1
+#define SLIDING_WINDOW_SIZE_S 2
+#define TIME_BETWEEN_GENERATED_PACKAGES_MS 10
+#define TIME_BETWEEN_PRINTS_S 1
 
-typedef struct message {
+// Errors in this program are sparcely handled.
+
+// I would have liked to have used different files but I'm not sure how to appropiately modify the Dockerfile
+
+// I tried a different code formatting for this code.
+
+typedef struct message
+{
 	int sender_id, data, sequence_number;
 } message;
 
-typedef struct node_package node_package;
-
-struct node_package {
+/* We handle the list of recently recieved nodes as a doubly-linked-list, to easily add from head and
+remove from tail. */
+typedef struct node_recieved node_recieved;
+struct node_recieved
+{
 	int sender_id;
 	time_t timestamp;
-	node_package* next;
-	node_package* previous;
-} ;
+	node_recieved *next;
+	node_recieved *previous;
+};
 
-void handle_error(char * error_message) {
+void handle_error(char *error_message)
+{
 	perror(error_message);
 	exit(EXIT_FAILURE);
 }
 
-// Errors in this program are sparcely handled.
-// I would have liked to have used different files but I'm not sure how to appropiately modify the Dockerfile
+int msleep(long msec)
+{
+	struct timespec ts;
+	int res;
 
-void delete_list(node_package * node) {
-	if (node != NULL) {
+	ts.tv_sec = msec / 1000;
+	ts.tv_nsec = (msec % 1000) * 1000000;
+
+	do
+	{
+		res = nanosleep(&ts, &ts);
+	} while (res && errno == EINTR);
+
+	return res;
+}
+
+void delete_list(node_recieved *node)
+{
+	if (node != NULL)
+	{
 		delete_list(node->next);
 		free(node);
 	}
 }
 
-int count_sender_in_list(node_package * node, int sender_id) {
-	if (node == NULL) return 0; 
-	return ((node->sender_id == sender_id)?1:0) + count_sender_in_list(node->next, sender_id);
+int count_sender_in_list(node_recieved *node, int sender_id)
+{
+	if (node == NULL)
+		return 0;
+	return ((node->sender_id == sender_id) ? 1 : 0) + count_sender_in_list(node->next, sender_id);
 }
 
-void traffic_analyzer(int* pipe) {
+void traffic_analyzer(int *pipe)
+{
 	int ret, sender_id;
 	long int timer, time_of_last_print = time(NULL);
 	message message;
 	ret = close(pipe[1]);
-	if (ret < 0) handle_error("close");
-	
-	node_package *head = NULL, *tail = NULL, *new_node, *old_tail;
-	while (1) {
+	if (ret < 0)
+		handle_error("close");
+
+	node_recieved *head = NULL, *tail = NULL, *new_node, *old_tail;
+	while (1)
+	{
 		// If enough time has passed, print the current status
-		if (time(&timer) - time_of_last_print > TIME_BETWEEN_PRINTS) {
-			for (int i = 0; i<NUMBER_OF_NODES; i++) {
+		if (time(&timer) - time_of_last_print > TIME_BETWEEN_PRINTS_S)
+		{
+			for (int i = 0; i < NUMBER_OF_NODES; i++)
+			{
 				printf("%d: %03d ", i, count_sender_in_list(head, i));
 			}
 			printf("\n");
 			fflush(stdout);
-			
+
 			time_of_last_print = timer;
 		}
+
+		// Check whether a new package has been generated
 		ret = read(pipe[0], &sender_id, sizeof(sender_id));
-		if (ret < 0) handle_error("read");
-		if (ret == 0) {
+		if (ret < 0)
+			handle_error("read");
+		if (ret == 0)
+		{
 			delete_list(head);
 			_exit(EXIT_SUCCESS);
 		}
 
 		// Set up new node and add it as head
-		new_node = malloc(sizeof(node_package));
+		new_node = malloc(sizeof(node_recieved));
 		new_node->timestamp = time(&timer);
 		new_node->sender_id = sender_id;
 		new_node->next = head;
 		new_node->previous = NULL;
-		if (head == NULL) tail = new_node;
-		else head->previous = new_node;
+		if (head == NULL)
+			tail = new_node;
+		else
+			head->previous = new_node;
 		head = new_node;
 
 		// Delete old nodes from the list
-		while (timer - tail->timestamp > SLIDING_WINDOW_SIZE) {
+		while (timer - tail->timestamp > SLIDING_WINDOW_SIZE_S)
+		{
 			old_tail = tail;
 			tail->previous->next = NULL;
 			tail = tail->previous;
-			free(old_tail); 
-		} 
-			
+			free(old_tail);
+		}
 	}
-	
+
 	// We should never get here
 	handle_error("Unexpectedly got somewhere impossible");
 }
 
-void traffic_generator(int* pipe) {
-
-	int ret; 
+void traffic_generator(int *pipe)
+{
+	int ret;
 	srand(time(NULL));
 	ret = close(pipe[0]);
-	if (ret < 0) handle_error("close");
+	if (ret < 0)
+		handle_error("close");
 
-	while (1) {
-		usleep(TIME_BETWEEN_GENERATED_PACKAGES);
+	while (1)
+	{
+		msleep(TIME_BETWEEN_GENERATED_PACKAGES_MS);
 		int data = rand();
 		ret = write(pipe[1], &data, sizeof(data));
-		if (ret < 0) handle_error("write");
-		if (ret == 0) _exit(EXIT_SUCCESS); 
+		if (ret < 0)
+			handle_error("write");
+		if (ret == 0)
+			_exit(EXIT_SUCCESS);
 	}
 
 	// We should never get here
 	handle_error("Unexpectedly got somewhere impossible");
 }
 
-void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
+void broadcaster(int node_id, int *generator_pipe, int *analyzer_pipe)
+{
 
 	int ret;
 
@@ -129,43 +174,64 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 
 	int listening_fd;
 	struct sockaddr_in listening_address;
-	
+
 	listening_fd = socket(AF_INET, SOCK_DGRAM, 0);
-	
+
 	int reuseAddressPermission = 1;
-	ret = setsockopt(listening_fd, SOL_SOCKET, SO_REUSEADDR, (void *) &reuseAddressPermission, sizeof(reuseAddressPermission));
-    if (ret) handle_error("setsockopt");
-	
-	memset((char*) &listening_address, 0, sizeof(listening_address));
+	ret = setsockopt(listening_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&reuseAddressPermission, sizeof(reuseAddressPermission));
+	if (ret)
+		handle_error("setsockopt");
+
+	memset((char *)&listening_address, 0, sizeof(listening_address));
 	listening_address.sin_family = AF_INET;
 	listening_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	listening_address.sin_port = htons(PORT);
 
-	bind(listening_fd, (struct sockaddr *) &listening_address, sizeof(listening_address));
+	/*
+	This code may be useful but I'm not sure what it does, if I've implemented it correctly,
+	or whether the behaviour having it not commented out is intended.
+	The program seems to work without it, but it probably doesn't  implement the network topology as intended.
+	*/
 
-	fcntl(listening_fd, F_SETFL, O_NONBLOCK); //Set read as non blocking
+	/*
+	struct ifaddrs *addrs, *tmp;
+	getifaddrs(&addrs);
+	tmp = addrs;
+	while (tmp){
+		if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_PACKET) {
+			setsockopt(listening_fd, SOL_SOCKET, SO_BINDTODEVICE, tmp->ifa_name, sizeof(tmp->ifa_name));
+		}
+		tmp = tmp->ifa_next;
+	}
+	freeifaddrs(addrs);
+	*/
+
+	bind(listening_fd, (struct sockaddr *)&listening_address, sizeof(listening_address));
+
+	fcntl(listening_fd, F_SETFL, O_NONBLOCK); // Set read as non blocking
 
 	// OPEN WRITING SOCKET
-	
+
 	int writing_fd, n;
 	struct sockaddr_in broadcast_addr;
-		
+
 	memset(&broadcast_addr, 0, sizeof(broadcast_addr));
 	writing_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	int broadcastPermission = 1;
- 	ret = setsockopt(writing_fd, SOL_SOCKET, SO_BROADCAST, (void *) &broadcastPermission, sizeof(broadcastPermission));
-    if (ret) handle_error("setsockopt");
+	ret = setsockopt(writing_fd, SOL_SOCKET, SO_BROADCAST, (void *)&broadcastPermission, sizeof(broadcastPermission));
+	if (ret)
+		handle_error("setsockopt");
 
-	memset((char*) &broadcast_addr, 0, sizeof(broadcast_addr));
-	
+	memset((char *)&broadcast_addr, 0, sizeof(broadcast_addr));
+
 	broadcast_addr.sin_family = AF_INET;
 	broadcast_addr.sin_addr.s_addr = inet_addr(BROADCAST_ADDRESS);
 	broadcast_addr.sin_port = htons(PORT);
-		
-	int data, internal_sequence_number= -1;
+
+	int data, internal_sequence_number = -1;
 	message sending_message;
-	
+
 	// LISTENING LOOP
 	int sender_id, sequence_number;
 	message recieving_message;
@@ -173,40 +239,49 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 	printf("Node initialized\n");
 	fflush(stdout);
 
-	while(1) {
+	while (1)
+	{
 
 		// We check whether we have a new message ready (the read is non-blocking);
 		ret = read(generator_pipe[0], &data, sizeof(data));
 
-		if (ret > 0) {
+		if (ret > 0)
+		{
 
 			// Tell the analyzer that we recieved some data
 			ret = write(analyzer_pipe[1], &node_id, sizeof(node_id));
-			if (ret <= 0) handle_error("write");
-			
+			if (ret <= 0)
+				handle_error("write");
+
 			sending_message.data = data;
 			sending_message.sender_id = node_id;
 			sending_message.sequence_number = 0;
 
-			ret =sendto(writing_fd, &sending_message, sizeof(message), 0, (struct sockaddr*) &broadcast_addr, sizeof(broadcast_addr));
-			if (ret < sizeof(message)) handle_error("sendto");
+			ret = sendto(writing_fd, &sending_message, sizeof(message), 0, (struct sockaddr *)&broadcast_addr, sizeof(broadcast_addr));
+			if (ret < sizeof(message))
+				handle_error("sendto");
 		}
 
 		ret = read(listening_fd, &recieving_message, sizeof(message));
-		if (ret < 0) continue;
-		if (ret < sizeof(message)) handle_error("read");
+		if (ret < 0)
+			continue;
+		if (ret < sizeof(message))
+			handle_error("read");
 
 		sender_id = recieving_message.sender_id;
 		sequence_number = recieving_message.sequence_number;
 		data = recieving_message.data;
 
-		if (sender_id == node_id) continue;
+		if (sender_id == node_id)
+			continue;
 		// If the package's sequence number is higher than the internal one, discard it.
-		if (sequence_number>=internal_sequence_number && internal_sequence_number != -1) continue;
+		if (sequence_number >= internal_sequence_number && internal_sequence_number != -1)
+			continue;
 
 		// Tell the analyzer that we recieved some data
 		ret = write(analyzer_pipe[1], &sender_id, sizeof(sender_id));
-		if (ret <= 0) handle_error("write");
+		if (ret <= 0)
+			handle_error("write");
 
 		internal_sequence_number = sequence_number;
 
@@ -214,16 +289,19 @@ void broadcaster (int node_id, int* generator_pipe, int* analyzer_pipe) {
 		sending_message.sender_id = node_id;
 		sending_message.sequence_number = ++internal_sequence_number;
 
-		ret =sendto(writing_fd, &sending_message, sizeof(message), 0, (struct sockaddr*) &broadcast_addr, sizeof(broadcast_addr));
-		if (ret < 0) handle_error("sendto");
+		ret = sendto(writing_fd, &sending_message, sizeof(message), 0, (struct sockaddr *)&broadcast_addr, sizeof(broadcast_addr));
+		if (ret < 0)
+			handle_error("sendto");
 	}
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
 	setvbuf(stdout, NULL, _IONBF, 0);
 
-	if (argc < 2) {
+	if (argc < 2)
+	{
 		printf("You need to send the id of this node\n");
 		exit(EXIT_FAILURE);
 	}
@@ -231,22 +309,30 @@ int main(int argc, char* argv[]) {
 	int ret, generator_pipe[2], analyzer_pipe[2];
 
 	ret = pipe(generator_pipe);
-	if (ret < 0) handle_error("pipe");
-	fcntl(generator_pipe[0], F_SETFL, O_NONBLOCK); //Set read as non blocking
+	if (ret < 0)
+		handle_error("pipe");
+	fcntl(generator_pipe[0], F_SETFL, O_NONBLOCK); // Set read as non blocking
 	ret = pipe(analyzer_pipe);
-	if (ret < 0) handle_error("pipe");
+	if (ret < 0)
+		handle_error("pipe");
 
 	ret = fork();
-	if (ret < 0) handle_error("fork");
-	if (ret > 0) traffic_analyzer(analyzer_pipe);
+	if (ret < 0)
+		handle_error("fork");
+	if (ret > 0)
+		traffic_analyzer(analyzer_pipe);
 	ret = close(analyzer_pipe[0]);
-	if (ret < 0) handle_error("close");
+	if (ret < 0)
+		handle_error("close");
 
 	ret = fork();
-	if (ret < 0) handle_error("fork");
-	if (ret > 0) traffic_generator(generator_pipe);
+	if (ret < 0)
+		handle_error("fork");
+	if (ret > 0)
+		traffic_generator(generator_pipe);
 	ret = close(generator_pipe[1]);
-	if (ret < 0) handle_error("close");
+	if (ret < 0)
+		handle_error("close");
 
 	broadcaster(atoi(argv[1]), generator_pipe, analyzer_pipe);
 }
